fdf0578975dba047c2cdf392b3bcab9799f41dde
akshayinpoint <akshay@inpointtech.com>
2020-07-24 13:37:16 +0530

    Add trim_by_factor back

--- acquisition/core/trim.py
+++ acquisition/core/trim.py
@@ -2,11 +2,11 @@
 
 import os
 import random
-from typing import Union
+from typing import List, Union
 
 from moviepy.editor import VideoFileClip as vfc
 
-from acquisition.utils.local import temporary_copy
+from acquisition.utils.local import filename, temporary_copy
 
 #TODO(xames3): Update docstrings to match the latest argument
 # requirements.
@@ -80,3 +80,50 @@ def trim_sample_section(file: str,
   end = start + clip_length
   trim_video(temp, file, start, end)
   os.remove(temp)
+
+
+def trim_by_factor(file: str,
+                   factor: str = 's',
+                   clip_length: Union[float, int, str] = 30,
+                   last_clip: bool = True) -> List:
+  """Trims the video by deciding factor.
+
+  Trims the video as per the deciding factor i.e. trim by mins OR trim
+  by secs.
+  Args:
+    file: File to be used for trimming.
+    factor: Trimming factor (default: secs -> s) to consider.
+    clip_length: Length (default: 30) of each video clip.
+    last_clip: Boolean (default: True) value to consider the remaining
+               portion of the trimmed video.
+    codec: Codec (default: libx264 -> .mp4) to be used while trimming.
+    bitrate: Bitrate (default: min. 400) used while trimming.
+    fps: FPS (default: 24) of the trimmed video clips.
+    audio: Boolean (default: False) value to have audio in trimmed
+            videos.
+    preset: The speed (default: ultrafast) used for applying the
+            compression technique on the trimmed videos.
+    threads: Number of threads (default: 15) to be used for trimming.
+  """
+  clip_length = int(clip_length)
+  total_length = duration(file)
+  video_list = []
+  idx = 1
+  if factor == 'm':
+    start, end, clip_length = 0, clip_length * 60, clip_length * 60
+  else:
+    start, end = 0, clip_length
+  while clip_length < total_length:
+    trim_video(file, filename(file, idx), start, end)
+    video_list.append(filename(file, idx))
+    start, end, idx = end, end + clip_length, idx + 1
+    total_length -= clip_length
+  else:
+    if last_clip:
+      start, end = (duration(file) - total_length), duration(file)
+      trim_video(file, filename(file, idx), start, end)
+
+      if duration(filename(file, idx)) > (0.7 * clip_length):
+        video_list.append(filename(file, idx))
+
+  return video_list
